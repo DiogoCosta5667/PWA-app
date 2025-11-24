4e618aa6f2369a27c135fd2f9ee981c7a0beaf86
Diogo YT <cucojunior905@gmail.com>
2025-11-24 10:24:37 +0000

    Primeiro commit

---
+++ stadium/data/users/scopes.js
@@ -0,0 +1,21 @@
+module.exports= {
+    'Admin': 'admin',
+    'Member': 'member',
+    'NonMember': 'notMember',
+    'Anonimous': 'anonimous',
+}
+
+/*{
+    "name": "Diogo",
+    "email": "8240544@estg.ipp.pt",
+    "password": "1",
+    "role": {
+        "name": "Admin",
+        "scope": "admin"
+    },
+    "age": "19",
+    "address": "rua de exemplo",
+    "country": "Portugal",
+    "memberId": "memberId",
+    "tickets": "tickets"
+}*/ 
